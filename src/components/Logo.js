5ee0043450f49d88e050111fa60b728e60341ee4
r0bomurlok <igrtnsk@ya.ru>
2022-01-14 21:48:57 +0300

    feat: Добавить возможность переходить на главную страницу по клику на лого

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Logo.css';
+import { Link } from 'react-router-dom';
 
 function Logo () {
   return (
-    <p className="logo">
-      <span role="img" aria-label="Emoji critic logo">🤔</span> Emoji Critic
-    </p>
-  )
+		<Link to="/"><p className="logo">
+		<span role="img" aria-label="Emoji critic logo">🤔</span> Emoji Critic
+		
+	</p></Link>
+	
+		)
 }
 
 export default Logo
